0da777504b24d4f632d2200114858cc662ccac0a
Shijin_VR <shijin.eztn@gmail.com>
2026-09-16 21:16:18 +0530

    arm64 JDK fix, 3-worker standalone cluster, Iceberg on shared warehouse

--- startup.sh
+++ startup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
-
-# Activate virtual environment
 source /home/airflow/.venv/bin/activate
-
-# Start Airflow in the background
-airflow standalone &
-
-# Start Jupyter Lab in the foreground
-exec jupyter lab --allow-root --ip=0.0.0.0 --no-browser --IdentityProvider.token=''
+if [ "${START_AIRFLOW}" = "true" ]; then
+  airflow standalone &
+fi
+exec jupyter lab --allow-root --ip=0.0.0.0 --no-browser --IdentityProvider.token='' --notebook-dir=/home/airflow
